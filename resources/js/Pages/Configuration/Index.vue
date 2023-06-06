1a3692d0971b7666400b5deb576ece0e77b78daf
Masiré Fofana <masire.fofana@uipath.com>
2023-06-06 22:57:00 +0200

    Advanced configuration module

--- resources/js/Pages/Configuration/Index.vue
+++ resources/js/Pages/Configuration/Index.vue
@@ -52,6 +52,39 @@ import PageContentHeader from '@/Components/PageContentHeader.vue';
 
                 <div class="p-6 border-t border-gray-200 md:border-t-0">
                     <div class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                        </svg>
+                        <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                            <Link :href="route('configuration.ai-based-alert-triggers.index')">
+                                {{ __('AI based alert triggers') }}
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div class="ml-12">
+                        <div class="mt-2 text-sm text-gray-500">
+                            {{ __("Use AI to set up rules detecting uncommon behaviors in your digital workforce.") }}
+                        </div>
+
+                        <Link :href="route('configuration.ai-based-alert-triggers.index')">
+                            <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                                <div>{{ __('Configure') }}</div>
+
+                                <div class="ml-1 text-indigo-500">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                        <path fill-rule="evenodd"
+                                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+
+                <div class="p-6 border-t border-gray-200 md:border-r">
+                    <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="w-8 h-8 text-gray-400">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
@@ -84,23 +117,19 @@ import PageContentHeader from '@/Components/PageContentHeader.vue';
                     </div>
                 </div>
 
-                <div class="p-6 border-t border-gray-200 md:border-r">
+                <div class="p-6 border-t border-gray-200">
                     <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
                         </svg>
                         <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
-                            <Link :href="route('configuration.ai-based-alert-triggers.index')">
-                                {{ __('AI based alert triggers') }}
-                            </Link>
+                            <a href="#">
+                                {{ __('Advanced configuration') }}
+                            </a>
                         </div>
                     </div>
 
                     <div class="ml-12">
-                        <div class="mt-2 text-sm text-gray-500">
-                            {{ __("Use AI to set up rules detecting uncommon behaviors in your digital workforce.") }}
-                        </div>
-
                         <Link :href="route('configuration.ai-based-alert-triggers.index')">
                             <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
                                 <div>{{ __('Configure') }}</div>
@@ -116,7 +145,6 @@ import PageContentHeader from '@/Components/PageContentHeader.vue';
                         </Link>
                     </div>
                 </div>
-
                 <!-- <div class="p-6 border-t border-gray-200 md:border-t-0">
                     <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
